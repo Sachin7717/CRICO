1e3f9c2f1696a8e24b2f3bba76071d0a5f8d56cb
shashikant010 <shashikantyadav0503@gmail.com>
2024-09-13 23:03:35 +0530

    installed tailwind and write some portion of navbar

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,11 +1,27 @@
 import React from 'react'
+import { navItemsList } from '../constants'
 
-function navbar() {
+
+const NavItems =()=>(
+  <ul className='flex gap-10 mx-auto'>
+    {
+      navItemsList.map((item)=>(
+        <li key={item.id}>
+          {item.name}
+        </li>
+      ))
+    }
+  </ul>
+  
+)
+function Navbar() {
   return (
-    <div>
-      test
-    </div>
+    <nav className='flex justify-between text-xl my-2'>
+        <h1 className='mx-auto font-extrabold text-blue-600 font-serif'>CRICO</h1>
+        <NavItems/>
+    </nav>
+        
   )
 }
 
-export default navbar
+export default Navbar
---
+++ src/constants/index.js
@@ -0,0 +1,22 @@
+export const navItemsList =[
+    {
+        id: 1,
+        name: 'Home',
+        path: '/'
+    },
+    {
+        id: 2,
+        name:"Tournaments",
+        path:"/tournaments"
+    },
+    {
+        id:3,
+        name:"Matches",
+        path:"/matches"
+    },
+    {
+        id:4,
+        name:"About",
+        path:"/about"
+    }
+]
